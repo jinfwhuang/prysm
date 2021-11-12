7281f0e65e283533731e6e0ccf7acee8bcf5caaa
Jin Huang <huangjinf@gmail.com>
2021-11-11 17:38:03 -0800

    All in one commit

--- beacon-chain/gateway/helpers.go
+++ beacon-chain/gateway/helpers.go
@@ -25,6 +25,7 @@ func DefaultConfig(enableDebugRPCEndpoints bool, httpModules string) MuxConfig {
 			ethpbalpha.RegisterBeaconChainHandler,
 			ethpbalpha.RegisterBeaconNodeValidatorHandler,
 			ethpbalpha.RegisterHealthHandler,
+			ethpbalpha.RegisterLightClientHandler,
 		}
 		if enableDebugRPCEndpoints {
 			v1AlphaRegistrations = append(v1AlphaRegistrations, ethpbalpha.RegisterDebugHandler)
